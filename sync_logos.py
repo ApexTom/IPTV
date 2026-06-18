6c1a3e7dc5859e96eaaf25430ddecb58b6328ac6
Jack123liang <135587404+Jack123liang@users.noreply.github.com>
2026-06-18 15:50:09 +0800

    Update sync_logos.py

--- sync_logos.py
+++ sync_logos.py
@@ -5,7 +5,11 @@ import time
 # 台标下载源汇总
 LOGOS_TO_DOWNLOAD = {
     # === 央视 CCTV 系列：来自 fanmingming/live 公益台标库 ===
-    "TVB功夫.png": "https://gcore.jsdelivr.net/gh/taksssss/tv@main/icon/TVB功夫.png",
+    "Redbull.png": "https://github.com/tv-logo/tv-logos/blob/main/countries/international/red-bull-tv-int.png?raw=true",
+    "TRT_World.png": "https://github.com/tv-logo/tv-logos/blob/main/countries/turkey/trt-world-tr.png?raw=true"
+    "Movie_Sphere.png: "https://moviespheregold.com/assets/img/MovieSphereGold/Moviesphere-Gold-logo.png",
+    "HollyWire.png": "https://canalhollywood.pt/wp-content/themes/appx-hello-child/elementor-widgets/assets/logo-CHW-marca-negativo-duotono.svg",
+    "Wild_Earth.png": "https://upload.wikimedia.org/wikipedia/commons/6/68/CTV_Wild_Channel_Logo.png",
     "Cinemax.png": "https://tvlogo-282.pages.dev/logos/starhub/611_1920x1080_HTV.png",
     "LMN.png": "https://schedulesdirect-api20141201-logos.s3.dualstack.us-east-1.amazonaws.com/stationLogos/s18480_dark_360w_270h.png",
 }
